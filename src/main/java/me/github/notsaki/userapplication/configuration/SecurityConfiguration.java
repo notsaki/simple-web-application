05a32780791d629f9b3c3fd60e8595b06c1e0aca
Grigoris Tsakouridis <gr.tsakouridis@gmail.com>
2022-06-29 17:34:11 +0300

    Minor fixes on security

--- src/main/java/me/github/notsaki/userapplication/configuration/SecurityConfiguration.java
+++ src/main/java/me/github/notsaki/userapplication/configuration/SecurityConfiguration.java
@@ -1,60 +1,64 @@
 package me.github.notsaki.userapplication.configuration;
 
 import me.github.notsaki.userapplication.domain.service.TokenService;
+import me.github.notsaki.userapplication.dsl.HttpDsl;
 import me.github.notsaki.userapplication.filter.AuthenticationFilter;
 import me.github.notsaki.userapplication.filter.AuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
 	private final TokenService tokenService;
-	private final UserDetailsService userDetailsService;
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final AuthenticationConfiguration authenticationConfiguration;
 
 	public SecurityConfiguration(
-			UserDetailsService userDetailsService,
-			BCryptPasswordEncoder bCryptPasswordEncoder,
-			TokenService tokenService
+			TokenService tokenService,
+			AuthenticationConfiguration authenticationConfiguration
 	) {
-		this.userDetailsService = userDetailsService;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 		this.tokenService = tokenService;
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.userDetailsService)
-				.passwordEncoder(this.bCryptPasswordEncoder);
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().antMatchers("/login/**", "/token/**").permitAll();
-		http.authorizeRequests().antMatchers("/user/**").authenticated();
-		http.addFilterBefore(
-				new AuthenticationFilter(this.authenticationManagerBean()),
-				UsernamePasswordAuthenticationFilter.class
-		);
-		http.addFilterBefore(new AuthorizationFilter(this.tokenService), UsernamePasswordAuthenticationFilter.class);
+		this.authenticationConfiguration = authenticationConfiguration;
 	}
 
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.apply(HttpDsl.create());
+
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.authorizeRequests()
+				.antMatchers("/login/**", "/token/**")
+				.permitAll();
+
+		http.authorizeRequests()
+				.antMatchers("/user/**")
+				.authenticated();
+
+		http.addFilterBefore(
+				new AuthenticationFilter(this.authenticationManager()),
+				UsernamePasswordAuthenticationFilter.class
+		);
+
+		http.addFilterBefore(
+				new AuthorizationFilter(this.tokenService),
+				UsernamePasswordAuthenticationFilter.class
+		);
+
+		return http.build();
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager() throws Exception {
+		return this.authenticationConfiguration.getAuthenticationManager();
 	}
 }
